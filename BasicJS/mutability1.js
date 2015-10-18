975f8d393432207d00eece5828543bce4c0607a0
Yaxian <guyaxian21@163.com>
2015-10-18 13:26:31 +0800

    modify mutability1.js to show difference between i-- and --i

--- BasicJS/mutability1.js
+++ BasicJS/mutability1.js
@@ -1,8 +1,10 @@
 /* add an attribute to a function */
 
 function displayError(msg) {
+	console.log('displayError.numTimesExcuted before ++ operator is', displayError.numTimesExcuted);
 	displayError.numTimesExcuted++;
-	console.log(msg);
+	console.log('displayError.numTimesExcuted after ++ operator is', displayError.numTimesExcuted);
+	console.log('messages: ', msg);
 }
 
 displayError.numTimesExcuted = 0;
@@ -11,7 +13,18 @@ var limit = 10;
 while(limit--) {
 	displayError("Error "+limit);
 }
-console.log(displayError.numTimesExcuted); // 10
-
+console.log('The last displayError.numTimesExcuted is:',displayError.numTimesExcuted); // 10
 console.log(displayError.prototype); // {}
 console.log(displayError.hasOwnProperty("numTimesExcuted")); //true
+console.log("-------------------------------------------------");
+var limit2 = 10;
+console.log('limit2 = 10');
+while (--limit2) {
+  console.log('--limit2 operator: ', limit2);
+}
+console.log("-------------------------------------------------");
+var limit3 = 10;
+console.log('limit3 = 10');
+while (limit3--) {
+  console.log('limit3-- operator: ', limit3);
+}
